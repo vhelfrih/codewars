5efbe2b60138fb09c6f52e10c3400498c1b67bbb
Viktor <vhelfrih@gmail.com>
2022-11-26 06:59:32 +0100

    .

---
+++ 7 kyu - Filter Coffee.js
@@ -0,0 +1,20 @@
+// 7 kyu - Filter Coffee
+// You love coffee and want to know what beans you can afford to buy it.
+
+// The first argument to your search function will be a number which represents your budget.
+
+// The second argument will be an array of coffee bean prices.
+
+// Your 'search' function should return the stores that sell coffee within your budget.
+
+// The search function should return a string of prices for the coffees beans you can afford. The prices in this string are to be sorted in ascending order.
+
+function search(budget, prices) {
+  let res = [];
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] <= budget) {
+      res.push(prices[i]);
+    }
+  }
+  return res.sort((a, b) => a - b).toString();
+}
